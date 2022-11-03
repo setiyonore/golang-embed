99a9bf1472a6091211bf6ddbdd3bcbff3c0c8c8a
stynore <setiyono.ressly@gmail.com>
2022-11-03 07:35:09 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+)
+
+//go:embed version.txt
+var version string
+
+//go:embed baju.jpeg
+var baju []byte
+
+//go:embed files/*.txt
+var path embed.FS
+
+func main() {
+	fmt.Println(version)
+	err := ioutil.WriteFile("baju_new.jpeg", baju, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	dirEntries, _ := path.ReadDir("files")
+	for _, entry := range dirEntries {
+		if !entry.IsDir() {
+			fmt.Println(entry.Name())
+			file, _ := path.ReadFile("files/" + entry.Name())
+			fmt.Println(string(file))
+
+		}
+	}
+}
